bb39e9b9accf1ee729d8374db7daac0377a515b6
Kelly Whiting <kwhiting@gmail.com>
2017-08-08 16:21:03 -0700

    added dynamic port

--- server/index.js
+++ server/index.js
@@ -11,6 +11,6 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3000, function() {
-  console.log('listening on port 3000');
+app.listen(process.env.PORT, function() {
+  console.log('listening on port ' + process.env.PORT);
 });
