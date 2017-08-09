d54c7cc68a6539ad880f615aa3ccd59e1366297f
Kelly Whiting <kwhiting@gmail.com>
2017-08-09 16:17:36 -0700

    server skeleton created with fixes

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-// import List from './components/List.jsx';
 
 class SignIn extends React.Component {
   constructor(props) {
