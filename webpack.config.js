5e1cdfac3a4c3947577b9a290206d1b04e53b4cb
mkchang <martinkchang@gmail.com>
2017-08-10 00:13:05 -0700

    Remove git merge artifacts that were committed to master

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,12 @@ var SRC_DIR = path.join(__dirname, '/react-client/src');
 var DIST_DIR = path.join(__dirname, '/react-client/dist');
 
 module.exports = {
-<<<<<<< HEAD
-  entry: `${SRC_DIR}/index.jsx`,
-=======
   entry: {
     index: `${SRC_DIR}/index.jsx`,
     loggedIn: `${SRC_DIR}/loggedIn.jsx`,
   },
->>>>>>> Update html and jsx files, and webpack
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: DIST_DIR
   },
   module : {
