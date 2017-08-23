189103de7c47b43bddadfc3db5cd57276791d5b8
Ryan <Ryan@Toms-MacBook.local>
2017-08-23 11:31:43 -0700

    user profile component created and added to nav bar

---
+++ react-client/src/components/loggedInComponents/YourProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var YourProfile = () => (
+  <div className="about" >
+    <br></br>
+    <h1>
+      USER PROFILE GOES HERE!
+    </h1>
+  </div>
+);
+
+
+export default YourProfile;
